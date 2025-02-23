2be69f5b347d2743052448837c46b85878a6c244
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-02-23 20:16:11 +0000

    Chapter 6 Finished

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -1,28 +1,38 @@
 from odoo import fields, models
+from datetime import date, timedelta
 
 class Property(models.Model):
     _name = "estate.property"
     _description = "Real Estate Property"
     _order = "name"
 
-    name = fields.Char('Property Name', required=True)
+    name = fields.Char('Property Name', required=True, default='Unknown')
     description = fields.Char('Description')
     postcode = fields.Char('Postcode')
-    date_availability = fields.Date('Available From')
+    date_availability = fields.Date('Available From', default=lambda self: date.today() + timedelta(days=90))
     expected_price = fields.Float('Expected Price')
     selling_price = fields.Float('Selling Price')
-    bedrooms = fields.Integer('Bedrooms')
+    bedrooms = fields.Integer('Bedrooms', default=2)
+    last_seen = fields.Datetime("Last Seen", default=fields.Datetime.now)
     living_area = fields.Integer('Living Area')
     facades = fields.Integer('Facades')
     garage = fields.Boolean('Garage')
     garden = fields.Boolean('Garden')
     garden_area = fields.Integer('Garden Area')
+    active = fields.Boolean('Active', default=True)
     garden_orientation = fields.Selection([
         ('north', 'North'),
         ('south', 'South'),
         ('east', 'East'),
         ('west', 'West')
     ], 'Garden Orientation')
+    state = fields.Selection([
+        ('new', 'New'),
+        ('offer_received', 'Offer Received'),
+        ('offer_accepted', 'Offer Accepted'),
+        ('sold', 'Sold'),
+        ('canceled', 'Canceled')
+    ], 'Status', default='new')
 
     _sql_constraints = [
         ('name_description_check',
