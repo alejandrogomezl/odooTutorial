94204ac749279951d3096b10fc7fd48b82231c09
alejandrogomezl <alejandrogomezlozano@hotmail.com>
2025-02-25 10:13:55 +0000

    Chapter 11: Added propertys to poperty type

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -6,6 +6,7 @@ class PropertyType(models.Model):
     _order = "name"
 
     name = fields.Char('Property Type', required=True)
+    property_ids = fields.One2many('estate.property', 'property_type_id', 'Properties')
 
     _sql_constraints = [
         ('unique_property_type_name', 'UNIQUE(name)', 'The property type name must be unique.'),
